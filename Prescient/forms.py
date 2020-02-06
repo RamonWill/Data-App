438a58796adec5b0584ccce13bd6bb7e894f648f
RamonWill <ramonwilliams@hotmail.co.uk>
2020-02-06 21:00:25 +0000

    new watchlist form

--- Prescient/forms.py
+++ Prescient/forms.py
@@ -1,5 +1,4 @@
-from wtforms import Form, StringField, PasswordField, TextField, SubmitField, DecimalField, validators
-
+from wtforms import Form, StringField, PasswordField, TextAreaField, IntegerField, SelectField, DecimalField, validators
 
 class RegistrationForm(Form):
     username = StringField("Username", [validators.Length(min=4, max=25)])
@@ -10,3 +9,12 @@ class RegistrationForm(Form):
 class LoginForm(Form):
     username = StringField("Username", validators=[validators.DataRequired()])
     password = PasswordField("Password", validators=[validators.DataRequired()])
+
+
+class WatchlistForm(Form):
+
+    ticker = StringField("Ticker", validators=[validators.Length(min=3, max=20), validators.DataRequired()])
+    quantity = IntegerField("Quantity", validators=[validators.InputRequired(), validators.NumberRange(min=-10000000, max=10000000)])
+    price = DecimalField("Price", validators=[validators.InputRequired(), validators.NumberRange(min=0, max=100000)])
+    sector = SelectField("Sector",  validators=[validators.InputRequired()])  # From database
+    comments = TextAreaField("Comments", validators=[validators.Optional(), validators.Length(max=140)])
