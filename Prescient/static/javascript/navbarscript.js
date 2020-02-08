8761c6939545a7c62efa6e6069c8303fa9e43bed
RamonWill <ramonwilliams@hotmail.co.uk>
2020-02-08 17:31:58 +0000

    html and css changes

--- Prescient/static/javascript/navbarscript.js
+++ Prescient/static/javascript/navbarscript.js
@@ -2,6 +2,7 @@ const navSlide = () => {
   const breaks = document.querySelector('.breaks');
   const nav = document.querySelector('.nav-icons');
   const navIcons = document.querySelectorAll('.nav-icons li');
+  const section = document.getElementsByClassName('main-content');
   //toggle Nav
   breaks.addEventListener('click',()=>{
     nav.classList.toggle('nav-active');
@@ -14,7 +15,9 @@ const navSlide = () => {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index/10+0.5}s`;
       }
     });
-    // breaks animation
+    section[0].classList.toggle('main-content-active');
+
+    // 3break lines animation
     breaks.classList.toggle('toggle');
   });
 
