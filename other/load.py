b66ba883557b7df1481e3ec867c21bbb468d619e
RamonWill <ramonwilliams@hotmail.co.uk>
2020-02-12 02:11:36 +0000

    files to get data from database

--- other/load.py
+++ other/load.py
@@ -7,14 +7,10 @@ sectors = pd.read_csv(r"other\FTSE Sectors.csv")
 ticker_list = pd.read_csv(r"other\stock_tickers_av.csv")
 
 conn = sqlite3.connect(r"instance\MainDB.sqlite")
-c = conn.cursor()
 # writes the dataframes to SQL
 sectors.to_sql("sector_definitions", conn, if_exists="replace", index=False)
 ticker_list.to_sql("available_securities", conn, if_exists="replace", index=False)
 
-conn.commit()
-c.close()
-conn.close()
 
 # The below was done to check choices
 # conn = sqlite3.connect(r"instance\MainDB.sqlite")
