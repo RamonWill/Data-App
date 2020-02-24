a5e99e62567e796dd4100c98bb2dedd78070e291
RamonWill <ramonwilliams@hotmail.co.uk>
2020-02-24 13:45:52 +0000

    new feature: world map view of positions

--- Prescient/views/dashboard.py
+++ Prescient/views/dashboard.py
@@ -9,6 +9,37 @@ from Prescient.models import Watchlist_Group
 
 bp = Blueprint("dashboard", __name__)
 
+## DELETE SOON
+import plotly
+import pandas as pd
+import sqlite3
+import plotly.graph_objects as go
+import json
+## DELETE SOON
+
+def worldmap(user_id, group_id):
+    conn = sqlite3.connect(r"C:\Users\Owner\Documents\Data-App\Prescient\MainDB.db")
+    query = """Select COUNT(b.country) as 'No. of Positions', b.country as 'Country', b.ISO_alpha3_codes as 'ISO Code'
+    from watchlist_securities a, available_securities b
+    where b.ticker=a.ticker and user_id=:user_id and group_id=:group_id
+    GROUP BY b.country
+    """
+    params = {"user_id": user_id, "group_id":group_id}
+
+    df = pd.read_sql_query(query, conn, params=params)
+    fig = dict(data=[go.Choropleth(locations=df["ISO Code"],
+                                   z=df["No. of Positions"],
+                                   text=df["Country"],
+                                   colorscale="dense")],
+                                   layout=dict(title="No. of Positions by Country",
+                                               scene=dict(bgcolor='rgb(23,4,55)'),
+                                                          paper_bgcolor='rgba(0,0,0,0)',
+                                               font=dict(size = 14,
+                                                         color = "#FFFFFF")))
+
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
 def get_group_names(user_id):
     names = Watchlist_Group.query.filter_by(user_id=user_id).all()
@@ -46,14 +77,16 @@ def index():
         selection_id = get_group_id(selection, user_id)
         summary = Portfolio_Summaries(user_id, selection_id).summary_table()
         user_details = Portfolio_Performance(user_id, selection_id)
+        map = worldmap(user_id, selection_id)
         line_chart = user_details.summed_table()
         pie_chart = user_details.get_pie_chart()
         bar_chart = user_details.get_bar_chart()
-        return render_template("securities/dashboard.html", summary=summary, line_chart=line_chart, pie_chart=pie_chart, bar_chart=bar_chart, user_watchlists=user_watchlists, group_name=selection)
+        return render_template("securities/dashboard.html", summary=summary, line_chart=line_chart, pie_chart=pie_chart, bar_chart=bar_chart, user_watchlists=user_watchlists, group_name=selection, map=map)
 
     summary = Portfolio_Summaries(user_id, watchlist_id).summary_table()
     user_details = Portfolio_Performance(user_id, watchlist_id)
     line_chart = user_details.summed_table()
     pie_chart = user_details.get_pie_chart()
     bar_chart = user_details.get_bar_chart()
-    return render_template("securities/dashboard.html", summary=summary, line_chart=line_chart, pie_chart=pie_chart, bar_chart=bar_chart, user_watchlists=user_watchlists, group_name=first_watchlist_name)
+    map = worldmap(user_id, watchlist_id)
+    return render_template("securities/dashboard.html", summary=summary, line_chart=line_chart, pie_chart=pie_chart, bar_chart=bar_chart, user_watchlists=user_watchlists, group_name=first_watchlist_name, map=map)
