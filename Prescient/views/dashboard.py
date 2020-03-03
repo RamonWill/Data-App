81530fe13ac8735698be61ec06f2b01e4f6549b3
RamonWill <ramonwilliams@hotmail.co.uk>
2020-03-03 02:14:16 +0000

    created new view functions to reference and link to new class structure

--- Prescient/views/dashboard.py
+++ Prescient/views/dashboard.py
@@ -1,10 +1,10 @@
-from Prescient import db
+from Prescient import db, app
 from flask import (Blueprint,
                    render_template,
                    request)
 from flask_login import login_required, current_user
 from werkzeug.exceptions import abort
-from Prescient.database_tools.Extracts import Portfolio_Performance, PositionSummary, DashboardCharts
+from Prescient.database_tools.Extracts import Portfolio_Summary, PositionSummary, DashboardCharts
 from Prescient.models import Watchlist_Group, WatchlistItems, Available_Securities
 from sqlalchemy.sql import func
 from sqlalchemy.orm import aliased
@@ -60,6 +60,30 @@ def get_tickers(user_id, group_id):
 
     return [item.ticker for item in tickers]
 
+def get_trade_histroy(user_id, group_id, ticker):
+    params = {"user_id": user_id, "group_id": group_id, "ticker": ticker}
+    all_trades = WatchlistItems.query.\
+                 with_entities(WatchlistItems.ticker, WatchlistItems.quantity, WatchlistItems.price, func.date(WatchlistItems.created_timestamp).label("date")).\
+                 filter_by(**params).\
+                 order_by(WatchlistItems.created_timestamp).all()
+    return all_trades
+
+def get_flows(user_id, group_id):
+    params = {"user_id": user_id, "group_id": group_id}
+    w = aliased(WatchlistItems)
+    flows = WatchlistItems.query.\
+            with_entities(func.date(w.created_timestamp).label("index"), func.sum(w.quantity*w.price).label("flow")).\
+            filter_by(**params).\
+            group_by(func.date(w.created_timestamp)).\
+            order_by(w.created_timestamp).all()
+    return flows
+
+def get_market_prices(ticker):
+    connection = db.get_engine(app, "Security_PricesDB").connect()
+    query = f"SELECT * FROM '{ticker}'"
+    prices = connection.execute(query).fetchall()
+    connection.close()
+    return prices
 
 def get_position_summary(user_id, group_id):
     all_tickers = get_tickers(user_id, group_id)
@@ -74,7 +98,20 @@ def get_position_summary(user_id, group_id):
         summary = PositionSummary(trade_history, ticker).get_summary()
         if summary.quantity != 0:
             summary_table.append(summary)
+
+    if len(summary_table) > 7:
+        summary_table = summary_table[0:7]
     return summary_table
+
+def get_portfolio_summary(user_id, group_id):
+    Portfolio = Portfolio_Summary()
+    tickers = get_tickers(user_id, group_id)
+    for ticker in tickers:
+        prices = get_market_prices(ticker)
+        trades = get_trade_histroy(user_id, group_id, ticker)
+        Portfolio.add_position(prices, trades, ticker)
+    return Portfolio
+
 
 def get_group_id(watchlist, user_id):
     group_id = Watchlist_Group.query.filter_by(name=watchlist, user_id=user_id).first()
@@ -97,26 +134,34 @@ def index():
     else:
         first_watchlist_name = user_watchlists[0]
         watchlist_id = get_group_id(first_watchlist_name, user_id)
-
+    flows = get_flows(user_id, watchlist_id)
     if request.method == "POST":
         selection = request.form.get('watchlist_group_selection')
         selection_id = get_group_id(selection, user_id)
+        flows = get_flows(user_id, selection_id)
         summary = get_position_summary(user_id, selection_id)
-        if len(summary) > 7:
-            summary = summary[0:7]
-        user_details = Portfolio_Performance(user_id, selection_id)
+
+        Portfolio = get_portfolio_summary(user_id, selection_id)
+        portfolio_breakdown = Portfolio.portfolio_breakdown
+        portfolio_performance = Portfolio.generate_hpr(flows)
+        Charts = DashboardCharts()
+        pie_chart = Charts.get_pie_chart(portfolio_breakdown)
+        bar_chart = Charts.get_bar_chart(portfolio_breakdown)
         map = get_worldmap(user_id, selection_id)
-        line_chart = user_details.summed_table()
-        pie_chart = user_details.get_pie_chart()
-        bar_chart = user_details.get_bar_chart()
+        line_chart = portfolio_performance
+
         return render_template("securities/dashboard.html", summary=summary, line_chart=line_chart, pie_chart=pie_chart, bar_chart=bar_chart, user_watchlists=user_watchlists, group_name=selection, map=map)
 
     summary = get_position_summary(user_id, watchlist_id)
-    if len(summary) > 7:
-        summary = summary[0:7]
-    user_details = Portfolio_Performance(user_id, watchlist_id)
-    line_chart = user_details.summed_table()
-    pie_chart = user_details.get_pie_chart()
-    bar_chart = user_details.get_bar_chart()
+
+    Portfolio = get_portfolio_summary(user_id, watchlist_id)
+    portfolio_breakdown = Portfolio.portfolio_breakdown
+    Charts = DashboardCharts()
+    pie_chart = Charts.get_pie_chart(portfolio_breakdown)
+    bar_chart = Charts.get_bar_chart(portfolio_breakdown)
+    portfolio_performance = Portfolio.generate_hpr(flows)
+
+    line_chart = portfolio_performance
+
     map = get_worldmap(user_id, watchlist_id)
     return render_template("securities/dashboard.html", summary=summary, line_chart=line_chart, pie_chart=pie_chart, bar_chart=bar_chart, user_watchlists=user_watchlists, group_name=first_watchlist_name, map=map)
