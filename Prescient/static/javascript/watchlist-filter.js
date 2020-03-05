f30e129e35ddd58e6f5691eb7cb6a88e415aa5ee
RamonWill <ramonwilliams@hotmail.co.uk>
2020-03-05 16:09:28 +0000

    new feature: filter data by watchlist rows

---
+++ Prescient/static/javascript/watchlist-filter.js
@@ -0,0 +1,12 @@
+var $rows = $('#all-trades tbody tr');
+$('#filter').keyup(function() {
+
+    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+        reg = RegExp(val, 'i'),
+        text;
+
+    $rows.show().filter(function() {
+        text = $(this).text().replace(/\s+/g, ' ');
+        return !reg.test(text);
+    }).hide();
+});
