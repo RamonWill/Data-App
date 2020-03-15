b9d9df7b9264b3f2ba356328924ff59efc9aa980
RamonWill <ramonwilliams@hotmail.co.uk>
2020-03-15 23:51:23 +0000

    fixed filter bug. both inputs filtered only one table previously

--- Prescient/static/javascript/watchlist-all-security-filter.js
+++ Prescient/static/javascript/watchlist-all-security-filter.js
@@ -1,12 +1,12 @@
-var $rows1 = $('#all-trades tbody tr');
-$('#filter').keyup(function() {
+var $rows = $('#all-tradeable-securities tbody tr');
+$('#filter-tradeable-securities').keyup(function() {
 
     // Regular Expression
     var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
         reg = RegExp(val, 'i'),
         text;
 
-    $rows1.show().filter(function() {
+    $rows.show().filter(function() {
         text = $(this).text().replace(/\s+/g, ' ');
         return !reg.test(text);
     }).hide();
